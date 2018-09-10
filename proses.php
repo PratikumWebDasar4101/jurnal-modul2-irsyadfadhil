4932586f9a912725daef23626713e8a7ac7ee890
irsyadfadhil <42725274+irsyadfadhil@users.noreply.github.com>
2018-09-10 09:58:24 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,10 @@
 <?php 
-//Tuliskan kode anda di bawah ini
-
+	error_reporting(0);
+	$angka = $_GET['angka'];
+	for ($i=0; $i < $angka ; $i++) { 
+		for ($j=$i; $j < $angka; $j++) { 
+			echo "*";
+		}
+		echo "<br>";	
+	}
 ?>
