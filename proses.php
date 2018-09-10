d3b33ca5c35e6afcaa6587a1f24cacf115f01226
irsyadfadhil <42725274+irsyadfadhil@users.noreply.github.com>
2018-09-10 10:13:28 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -7,4 +7,19 @@
 		}
 		echo "<br>";	
 	}
+	$b=0;
+	$b1=0;
+	//bilangan genap
+	echo "genap = ";
+	for ($i=0; $i < $angka ; $i++) { 
+		$b = ($b+2);
+		echo $b;
+	}
+	echo "<br>";
+	//bilangan ganjil
+	echo "ganjil = ";
+	for ($i=0; $i < $angka ; $i++) { 
+		$b1 = ($b1+2);
+		echo $b1-1;
+	}
 ?>
